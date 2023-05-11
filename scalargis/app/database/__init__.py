3bbd1ae8ea90041c0367d8f39bc4b21e5f8d38e0
Fernando Ribeiro <fernandinand@gmail.com>
2023-05-11 10:50:47 +0100

    Rework database initialization for stack runtime compatibility

--- scalargis/app/database/__init__.py
+++ scalargis/app/database/__init__.py
@@ -1,5 +1,18 @@
-from flask import current_app
+import logging
+import sys
+
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+log = logging.getLogger(__name__)
 
+try:
+    db = SQLAlchemy()
+except SQLAlchemy.ConnectionError:
+    log.error('Database connection error. Please check database...')
+    sys.exit(1)
+except SQLAlchemy.PermissionError:
+    log.error('Database permission error. Please grant user credentials...')
+    sys.exit(1)
+except Exception as e:
+    log.error("Generic exception: {}".format(e))
+    sys.exit(1)
