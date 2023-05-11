3eaf4d42c45d18d1f380a4943c6516c1fdc1343b
Fernando Ribeiro <fernandinand@gmail.com>
2023-05-11 13:01:29 +0100

    raise database init erros for container restart

--- scalargis/app/database/__init__.py
+++ scalargis/app/database/__init__.py
@@ -2,11 +2,15 @@ import logging
 import sys
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import OperationalError
 
 log = logging.getLogger(__name__)
 
 try:
     db = SQLAlchemy()
+except OperationalError:
+    log.error('Database connection error. Will retry...')
+    raise
 except SQLAlchemy.ConnectionError:
     log.error('Database connection error. Please check database...')
     sys.exit(1)
