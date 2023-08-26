012a55f202fad99b67f660e49b9c84e6a03f424a
ricardogsena <ricardo.sena@wkt.pt>
2023-08-26 23:57:18 +0100

    Add default locale config option

--- scalargis/app/modules/map/controllers.py
+++ scalargis/app/modules/map/controllers.py
@@ -4,6 +4,7 @@ import logging
 from flask import current_app, Response
 
 from . import mod
+from app.utils.settings import get_default_locale
 from app.utils.http import get_script_root, get_base_url
 
 
@@ -21,10 +22,12 @@ def index(path):
 
     root_path = get_script_root()
     base_url = get_base_url()
+    default_locale = get_default_locale()
 
     f = open(os.path.join(current_app.static_folder, 'viewer', 'index.html'), "r", encoding='utf-8')
     in_html = f.read()
     out_html = in_html.replace("__SCALARGIS_ROOT_PATH__", root_path)
     out_html = out_html.replace("__SCALARGIS_ROOT_PATH_BASE_URL__", base_url)
+    out_html = out_html.replace("__SCALARGIS_DEFAULT_LOCALE__", default_locale)
 
     return Response(out_html, mimetype='text/html')
