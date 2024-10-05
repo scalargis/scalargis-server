8e8e314be9d6482a73da655e6d2ab5a688475da0
ricardogsena <ricardo.sena@wkt.pt>
2024-10-05 23:56:44 +0100

    Pass TRUSTED_PROXY and URL_SCHEME env variable to waitress

--- scalargis/server.py
+++ scalargis/server.py
@@ -10,6 +10,8 @@ os.chdir(this_files_dir)
 port = int(os.environ.get('PORT')) if os.environ.get('PORT') else 5000
 url_prefix = os.environ.get('URL_PREFIX') or ''
 threads = int(os.environ.get('THREADS')) if os.environ.get('THREADS') else 6
+trusted_proxy = os.environ.get('TRUSTED_PROXY') or None
+url_scheme = os.environ.get('URL_SCHEME') or 'http'
 
 
 if __name__ == "__main__":
@@ -17,4 +19,5 @@ if __name__ == "__main__":
 
     init_wsgi()
 
-    serve(app, host='0.0.0.0', port=port, threads=threads, url_prefix=url_prefix)
+    serve(app, host='0.0.0.0', port=port, threads=threads, url_prefix=url_prefix, trusted_proxy=trusted_proxy,
+          url_scheme=url_scheme)
