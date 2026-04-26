539e139d46241aeccc9d5b497785fd99ceaeeaed
ricardogsena <ricardo.sena@wkt.pt>
2026-04-26 02:21:33 +0100

    add filter to intersects_layers pgpsql function

--- scalargis/app/plugins/spatial_toolbox/database/spatial_toolbox_functions.sql
+++ scalargis/app/plugins/spatial_toolbox/database/spatial_toolbox_functions.sql
@@ -1,148 +1,222 @@
 -- DROP FUNCTION {schema}.intersects_layers(json, text, int4, int4, int4);
 
-CREATE OR REPLACE FUNCTION {schema}.intersects_layers(_layers json, _geom_ewkt text, _out_srid integer, _buffer integer DEFAULT 0, _buffer_srid integer DEFAULT 3857)
- RETURNS jsonb
- LANGUAGE plpgsql
+CREATE OR REPLACE FUNCTION {schema}.intersects_layers(
+    _layers json,
+    _geom_ewkt text,
+    _out_srid integer,
+    _buffer integer DEFAULT 0,
+    _buffer_srid integer DEFAULT 3857
+)
+RETURNS jsonb
+LANGUAGE plpgsql
 AS $function$
 DECLARE
-	--data_srid integer;
-	g Geometry; -- = ST_Transform(p_geom_ewkt::Geometry, 3763);
+    g Geometry;
 
-	area_geom double precision = 0;
-	length_geom  double precision = 0;
-	_sql text;
-	_r record;
-	my_json jsonb;
-	result_json jsonb;
-	_i jsonb;
-	_ii jsonb;
+    area_geom double precision = 0;
+    length_geom double precision = 0;
+    _sql text;
+    _r record;
+    my_json jsonb;
+    result_json jsonb;
+    _i jsonb;
+    _ii jsonb;
 
-	_my_table_sql text;
-	_my_fields_sql text;
-	_id integer = 0;
-	_path text;
-	_in_layer_srid integer;
+    _my_table_sql text;
+    _my_fields_sql text;
+    _where_not_null_sql text;
+    _id integer = 0;
+    _path text;
+    _in_layer_srid integer;
+    _allow_null boolean;
 
 BEGIN
-	result_json = _layers; -- using input json as template
+    result_json = _layers; -- using input json as template
 
-	--data_srid = 3763;
-	-- raise notice 'Data SRID=%', data_srid;
-	--g = ST_Transform(_geom_ewkt::Geometry, data_srid);
-	--raise notice 'g=%', st_astext(g);
-	--area_geom = ST_Area(g);
+    IF _buffer > 0 THEN
+        _geom_ewkt = ST_MakeValid(
+            ST_Buffer(ST_Transform(_geom_ewkt::geometry, _buffer_srid), _buffer)
+        );
+    END IF;
 
-	if _buffer > 0 then
-	--_geom_ewkt = st_makevalid(ST_Buffer(st_transform(_geom_ewkt::geometry,3763), _buffer));
-	_geom_ewkt = st_makevalid(ST_Buffer(st_transform(_geom_ewkt::geometry, _buffer_srid), _buffer));
-	end if;
-
-
-	my_json = _layers->'layers';
+    my_json = _layers->'layers';
 	--raise notice 'my json=%', my_json;
 
-	For _i in select jsonb_array_elements(my_json) Loop
-
+    FOR _i IN SELECT jsonb_array_elements(my_json)
+    LOOP
 		--raise notice '_i=%', _i;
-		_my_table_sql = (_i->>'schema') || '.' || (_i->>'table'); -- create table name for sql exec
+        _my_table_sql = (_i->>'schema') || '.' || (_i->>'table');
 		--raise notice 'my_table=%', _my_table_sql;
-		_my_fields_sql = '';
+        _my_fields_sql = '';
+        _where_not_null_sql = ''; -- reset by layer
 
-		For _ii in select jsonb_array_elements(_i->'fields') Loop
-		--raise notice '_ii=%', _ii;
-		_my_fields_sql = _my_fields_sql || ',' || (_ii->>'field');
-		End Loop;
+        -- FIELDS LOOP
+        FOR _ii IN SELECT jsonb_array_elements(_i->'fields')
+        LOOP
+			--raise notice '_ii=%', _ii;
+            _my_fields_sql = _my_fields_sql || ',' || (_ii->>'field');
 
-		if length(_my_fields_sql) > 0 then -- need cut first char
-		_my_fields_sql = right(_my_fields_sql,-1);
-		end if;
+            -- check allowNull (default TRUE)
+            _allow_null := COALESCE((_ii->>'allowNull')::boolean, TRUE);
+
+            IF _allow_null = FALSE THEN
+                IF _where_not_null_sql <> '' THEN
+                    _where_not_null_sql := _where_not_null_sql || ' AND ';
+                END IF;
+
+                _where_not_null_sql := _where_not_null_sql ||
+                    format('%I IS NOT NULL', _ii->>'field');
+            END IF;
+        END LOOP;
+
+        IF length(_my_fields_sql) > 0 THEN  -- need cut first char
+            _my_fields_sql = right(_my_fields_sql, -1);
+        END IF;
 
 		--raise notice 'my_fields_sql=%', _my_fields_sql;
 
-		Select srid Into _in_layer_srid from geometry_columns where
-		f_table_schema like (_i->>'schema') and f_table_name like (_i->>'table') and f_geometry_column like (_i->>'geom_field');
+        -- SRID of layer
+        SELECT srid INTO _in_layer_srid
+        FROM geometry_columns
+        WHERE f_table_schema = (_i->>'schema')
+          AND f_table_name = (_i->>'table')
+          AND f_geometry_column = (_i->>'geom_field');
 		--raise notice '_in_layer_srid=%', _in_layer_srid;
 
-		if _in_layer_srid = 0 then
-		    EXECUTE format(
-			    'SELECT ST_SRID(%I) FROM %I.%I LIMIT1',
-		        _i->>'geom_field',
-				_i->>'schema',
-		        _i->>'table'
-		    )
-			INTO _in_layer_srid;
+        IF _in_layer_srid = 0 THEN
+            EXECUTE format(
+                'SELECT ST_SRID(%I) FROM %I.%I LIMIT 1',
+                _i->>'geom_field',
+                _i->>'schema',
+                _i->>'table'
+            ) INTO _in_layer_srid;
 			--raise notice '_in_layer_srid=%', _in_layer_srid;
-		end if;
+        END IF;
 
-		g = ST_Transform(_geom_ewkt::Geometry, _in_layer_srid);
-		--g = ST_Transform(_geom_ewkt::Geometry, data_srid);
+        g = ST_Transform(_geom_ewkt::Geometry, _in_layer_srid);
 
-		area_geom = ST_Area(g);
-		length_geom = ST_Length(g);
+        area_geom = ST_Area(g);
+        length_geom = ST_Length(g);
 		--raise notice 'g=%', g::text;
 
-		if length(_my_fields_sql) = 0 then
-			_sql = '
-			with t as(
-			SELECT ST_Union(ST_Intersection('|| (_i->>'geom_field') ||', '''|| g::text ||''')) as geom
-			FROM '|| _my_table_sql ||'
-			WHERE ST_INTERSECTS('|| (_i->>'geom_field') ||', '''|| g::text ||''')
-			)
-			select row_number() over () id, t.*,
-			ST_Dimension(geom) dimension, ST_ASEWKT(ST_Transform(geom,'|| _out_srid::text ||')) geom,
-			ST_Area(geom) area, ST_Length(geom) length,
-			CASE
-				WHEN ST_Dimension(geom) = 2 and '|| area_geom || ' > 0 THEN (ST_Area(geom) * 100)/'|| area_geom::text ||'
-				WHEN ST_Dimension(geom) = 1 and '|| length_geom || ' > 0 THEN (ST_Length(geom) * 100)/'|| length_geom::text ||'
-				ELSE 0::double precision END as percent
-			from t';
-		else
-			_sql = '
-			with t as(
-			SELECT '|| _my_fields_sql ||', ST_Union(ST_Intersection('|| (_i->>'geom_field') ||', '''|| g::text ||''')) as geom
-			FROM '|| _my_table_sql ||'
-			WHERE ST_INTERSECTS('|| (_i->>'geom_field') ||', '''|| g::text ||''')
-			GROUP BY '|| _my_fields_sql ||' ORDER BY '|| _my_fields_sql ||'
-			)
-			select row_number() over () id, t.*,
-			ST_Dimension(geom) dimension, ST_ASEWKT(ST_Transform(geom,'|| _out_srid::text ||')) geom,
-			ST_Area(geom) area, ST_Length(geom) length,
-			CASE
-				WHEN ST_Dimension(geom) = 2 and '|| area_geom || ' > 0 THEN (ST_Area(geom) * 100)/'|| area_geom::text ||'
-				WHEN ST_Dimension(geom) = 1 and '|| length_geom || ' > 0 THEN (ST_Length(geom) * 100)/'|| length_geom::text ||'
-				ELSE 0::double precision END as percent
-			from t';
-		end if;
+        -- BUILD FINAL WHERE CONDITION
+        -- base: ST_INTERSECTS
+        -- + NOT NULL (if exists)
+		-- + filter (if exists)
+        DECLARE
+            _where_sql text;
+			_layer_filter text;
+        BEGIN
+			_layer_filter := _i->>'filter';
+
+            _where_sql := format(
+                'ST_INTERSECTS(%I, %L)',
+                _i->>'geom_field',
+                g::text
+            );
+
+            IF _where_not_null_sql <> '' THEN
+                _where_sql := _where_sql || ' AND ' || _where_not_null_sql;
+            END IF;
+
+			IF _layer_filter IS NOT NULL AND trim(_layer_filter) <> '' THEN
+				_where_sql := _where_sql || ' AND (' || _layer_filter || ')';
+			END IF;
+
+            -- FINAL QUERY
+            IF length(_my_fields_sql) = 0 THEN
+                _sql = format('
+                    WITH t AS (
+                        SELECT ST_Union(ST_Intersection(%1$I, %2$L)) AS geom
+                        FROM %3$s
+                        WHERE %4$s
+                    )
+                    SELECT row_number() OVER () id, t.*,
+                        ST_Dimension(geom) dimension,
+                        ST_AsEWKT(ST_Transform(geom,%5$s)) geom,
+                        ST_Area(geom) area,
+                        ST_Length(geom) length,
+                        CASE
+                            WHEN ST_Dimension(geom) = 2 AND %6$s > 0 THEN (ST_Area(geom)*100)/%6$s
+                            WHEN ST_Dimension(geom) = 1 AND %7$s > 0 THEN (ST_Length(geom)*100)/%7$s
+                            ELSE 0 END AS percent
+                    FROM t',
+                    _i->>'geom_field',
+                    g::text,
+                    _my_table_sql,
+                    _where_sql,
+                    _out_srid,
+                    area_geom,
+                    length_geom
+                );
+            ELSE
+                _sql = format('
+                    WITH t AS (
+                        SELECT %1$s,
+                               ST_Union(ST_Intersection(%2$I, %3$L)) AS geom
+                        FROM %4$s
+                        WHERE %5$s
+                        GROUP BY %1$s
+                        ORDER BY %1$s
+                    )
+                    SELECT row_number() OVER () id, t.*,
+                        ST_Dimension(geom) dimension,
+                        ST_AsEWKT(ST_Transform(geom,%6$s)) geom,
+                        ST_Area(geom) area,
+                        ST_Length(geom) length,
+                        CASE
+                            WHEN ST_Dimension(geom) = 2 AND %7$s > 0 THEN (ST_Area(geom)*100)/%7$s
+                            WHEN ST_Dimension(geom) = 1 AND %8$s > 0 THEN (ST_Length(geom)*100)/%8$s
+                            ELSE 0 END AS percent
+                    FROM t',
+                    _my_fields_sql,
+                    _i->>'geom_field',
+                    g::text,
+                    _my_table_sql,
+                    _where_sql,
+                    _out_srid,
+                    area_geom,
+                    length_geom
+                );
+            END IF;
+        END;
 
 		--raise notice '_sql=%', _sql;
 
-		For _r In Execute _sql Loop
+        -- EXECUTE
+        FOR _r IN EXECUTE _sql LOOP
 			--raise notice '_r=%', jsonb_agg(_r)->>0; --> first element.
-			_path = '{layers,'|| _id::text ||',results}'; -- constructing path to results array
+            _path = '{layers,'|| _id::text ||',results}'; -- constructing path to results array
 			--raise notice 'result value before insert:%', result_json::jsonb#>_path::text[];
 
-			if jsonb_array_length(result_json::jsonb#>_path::text[]) = 0 Then
-				result_json = jsonb_set(result_json,_path::text[], jsonb_agg(_r));
+            IF jsonb_array_length(result_json#>_path::text[]) = 0 THEN
+                result_json = jsonb_set(result_json,_path::text[], jsonb_agg(_r));
 				--raise notice '(new) result_json=%', result_json;
-			else
-				result_json = jsonb_set(result_json,_path::text[], (result_json::jsonb#>_path::text[] || jsonb_agg(_r)));
+            ELSE
+                result_json = jsonb_set(
+                    result_json,
+                    _path::text[],
+                    (result_json#>_path::text[] || jsonb_agg(_r))
+                );
 				--raise notice '(add to) result_json=%', result_json;
-			end if;
-		End Loop;
+            END IF;
+        END LOOP;
 
-		_id=_id+1; -- id nb of element increment
+        _id = _id + 1; -- id nb of element increment
 		--raise notice '-----------------------------------------------------------------------------';
 
 		-- add geom to result (buffered)
-		result_json = jsonb_set(result_json,'{output_geom}'::text[], to_jsonb(ST_AsEWKT(st_transform(_geom_ewkt,_out_srid))));
+        result_json = jsonb_set(
+            result_json,
+            '{output_geom}',
+            to_jsonb(ST_AsEWKT(ST_Transform(_geom_ewkt,_out_srid)))
+        );
 
-	End Loop;
+    END LOOP;
 
-	Return jsonb_pretty(result_json);
-
+    RETURN jsonb_pretty(result_json);
 END
-$function$
-;
+$function$;
 
 -- Permissions
 
