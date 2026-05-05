29378acfe928686a173d1082dc0a604190134b9f
Miguel Silva <dznindev@gmail.com>
2026-05-05 10:42:28 +0100

    Revert "Remove Waitress trusted_proxy to fix conflict with ProxyFix"
    This reverts commit 86baeb4cfb9a4a32342a7a0ba300bb77370f8454.

--- scalargis/server.py
+++ scalargis/server.py
@@ -13,6 +13,7 @@ url_prefix = os.environ.get('URL_PREFIX') or ''
 threads = int(os.environ.get('THREADS')) if os.environ.get('THREADS') else 6
 channel_timeout = int(os.environ.get('CHANNEL_TIMEOUT')) if os.environ.get('CHANNEL_TIMEOUT') else 120
 connection_limit = int(os.environ.get('CONNECTION_LIMIT')) if os.environ.get('CONNECTION_LIMIT') else 100
+trusted_proxy = os.environ.get('TRUSTED_PROXY') or None
 url_scheme = os.environ.get('URL_SCHEME') or 'http'
 
 logger = logging.getLogger('scalargis')
@@ -29,5 +30,5 @@ if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=port, threads=threads,
           channel_timeout=channel_timeout,
           connection_limit=connection_limit,
-          url_prefix=url_prefix,
+          url_prefix=url_prefix, trusted_proxy=trusted_proxy,
           url_scheme=url_scheme)
